19717fd838c00f3ab54799183884ff05a0cb90fe
laith almouhtaseb <laithml95@gmail.com>
2024-07-31 21:31:26 +0300

    integrate the python backend

--- actions/model_actions.js
+++ actions/model_actions.js
@@ -1,27 +1,43 @@
 export const processImage = async (imageUri) => {
-    const url = 'http://172.20.10.2:8000/process-image/';  // Change this URL to your actual FastAPI server URL
+    const url = 'http://localhost:8000/upload-images';  // Change this URL to your actual FastAPI server URL
 
     try {
-        // Convert the image URI to blob since fetch API needs FormData with a blob for file upload
+        // Convert the image URI to a blob since fetch API needs FormData with a blob for file upload
         const response = await fetch(imageUri);
         const blob = await response.blob();
         const formData = new FormData();
-        formData.append('file', blob, 'image.jpg');  // Append the blob to FormData, 'image.jpg' is the filename that will be used on the server side
+        formData.append('files', {
+            uri: imageUri,
+            name: 'image.jpg',  // Adjust the name and extension as needed
+            type: 'image/jpeg',  // Adjust the MIME type as needed
+        });
 
         console.log('Sending image to server...');
-        console.log('response:', response);
-        console.log('Image URI:', imageUri);
-        console.log('Image blob:', blob);
+
         // Make the fetch request to the FastAPI server
         const fetchResponse = await fetch(url, {
             method: 'POST',
             body: formData,
+            headers: {
+                'Accept': 'application/json',
+            },
         });
+
+        console.log('Server response:', fetchResponse);
+
+        if (!fetchResponse.ok) {
+            const errorText = await fetchResponse.text();
+            console.error('Server responded with error:', errorText);
+            throw new Error(`Failed to process image. Server responded with: ${fetchResponse.status} ${fetchResponse.statusText}`);
+        }
 
         // Assuming the server responds with JSON data containing the processed results
         const json = await fetchResponse.json();
 
-        return json;  // Contains texts and images from the processed image
+        // Print the structure of the object
+        console.log('Processed JSON:', JSON.stringify(json, null, 2));
+
+        return json;
     } catch (error) {
         console.error('Error processing image:', error);
         throw new Error('Failed to process image.');
